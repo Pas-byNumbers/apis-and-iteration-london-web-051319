3db75ba824f2f2b697a1909e36a2bb3af85db593
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-14 12:15:43 +0000

    Automatically backed up by Learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,6 +6,17 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  
+  binding.pry
+  
+  response_hash.each do |header, cat|
+    header.each do |cat, result|
+      if header.include?("films")
+        puts header["films"]
+      end
+    end
+  end
+
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
